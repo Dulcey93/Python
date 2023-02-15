fb18d62a9a533e64d3ffaa90e7256ecfe5b73eae
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-02-15 18:22:52 -0500

    Ejercicio nueve

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-# Escriba un bloque cualquiera de código en Python en donde
-# utilice 2 condicionales (if) anidados.
+# Construya un algoritmo en Python, que permita ingresar la
+# información de un empleado e imprima el nombre, los
+# apellidos y la antigüedad. Los datos que se deben solicitar
+# son los siguientes:
+# *Nombre * Teléfono *Año de ingreso a la empresa
+# *Apellidos *Edad.
+import datetime
+option =0
+currentDateTime = datetime.datetime.now()
+date = currentDateTime.date()
+year = int(date.strftime("%Y"))
+class Person:
+    def __init__(self, nombre, apellido, edad, telefono, fecha_ingreso):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.telefono = telefono
+        self.fecha_ingreso = fecha_ingreso
 
-num1,num2,num3= 1,2,3
+    def imprimir(self):
+        antiguedad = year - int(self.fecha_ingreso)
+        return print(f"***===============> El empleado {self.nombre} {self.apellido} tiene {antiguedad} años en la empresa ///********** \t \n")
 
-if(num1<=num2):
-    if(num2<=num3):
-        print(f"{num3} es el mayor")
-    else: 
-        print(f"{num2} es mayor que {num3}")
-else:
-    print(f"{num1} es mayor que {num2}")
+while(option != 3):
+    option = int (input((f""" ====> MENÚ EMPLEADOS <====
+    \n 1) Ingresar empleado
+    \n 2) Imprimir empleado
+    \n 3) Salir
+    \n""")))
+
+    if(option==1):
+        print(f""" ====> INGRESAR EMPLEADO <====""")
+        nombre = input(f"Ingrese el nombre: ")
+        apellido = input(f"Ingrese el apellido: ")
+        edad = input(f"Ingrese la edad: ")
+        telefono = input(f"Ingrese el telefono: ")
+        fecha_ingreso = input(f"Ingrese la fecha de ingreso *EJEMPLO 2017*: ")
+        instancia = Person(nombre, apellido, edad, telefono, fecha_ingreso)
+    if(option==2):
+        instancia.imprimir()
