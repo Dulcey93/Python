dc647ba173d73c36bc359c7afade32b3ae0d6e0b
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-02-15 18:18:28 -0500

    Ejercicio uno

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1. Qué operadores utiliza Python en los siguientes casos:
+
+# A. División Modular
+# B. Exponenciación
+# C. División que retorne entero.
+
+num1, num2 = 5,6
+print("====> Respuesta N°1<====")
+print(f"B. Exponenciación {num1}**{num2} = {num1**num2}")
+print(f"A. División Modular {num1}%{num2} = {num1%num2}")
+print(f"C. División que retorne entero. {num1}//{num2} = {num1//num2}")
