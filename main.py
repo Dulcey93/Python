7d041e9dcfa6bf78addb53ded3cf140be64d8722
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-02-15 18:21:00 -0500

    Ejercicio cinco

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-# Que son las expresiones regulares en Python?
-""" Son esencialmente en un lenguaje de programación diminuto y altamente especializado incrustado dentro de Python y disponible a través del módulo re. Usando este pequeño lenguaje, especificas las reglas para el conjunto de cadenas de caracteres posibles que deseas hacer coincidir; este conjunto puede contener frases en inglés, o direcciones de correo electrónico, o comandos TeX, o cualquier cosa que desee. """
+# Enumere 5 tipos de datos en Python y suministre un valor de
+# ejemplo de cada uno.
 
-
-# Quizás el metacarácter más importante es la barra invertida, \. Al igual que en los literales de cadena de Python, la barra invertida puede ir seguida de varios caracteres para señalar varias secuencias especiales. 
-# Tomemos un ejemplo: \w coincide con cualquier carácter alfanumérico. Si el patrón de expresiones regulares se expresa en bytes, esto es equivalente a la clase [a-zA-Z0-9_]. Si el patrón de expresiones regulares es una cadena de caracteres, \w coincidirá con todos los caracteres marcados como letras en la base de datos Unicode proporcionada por el módulo unicodedata.
-""" fuente: https://docs.python.org/es/3/howto/regex.html """
-
-#Metacarácteres
-meta = ". ^ $ * + ? { } [ ] \ | ( )"
+nombre = "Santiago Dulcey"
+edad = 23
+altura = 1.75
+vacio= None
+soyEstudiante = True
+listaImutable = ("Hola", "pedro")
+pasaTiempo = ["Programar", "Jugar", "Tocar Ukelele", 10]
+listaRopa = {
+    "gorra": False,
+    "pantalon": "Blue" 
+}
+#listar el array
+for tiempo in pasaTiempo:
+    print(f"\t Hola soy un pasaTiempo: {tiempo}")
+#Obtener la posición    
+for (index,value) in enumerate(pasaTiempo):
+    print(f"esta es la posición: {index}")
+print(listaRopa.get("pantalon"))
+print("".join(list(listaImutable)))
+print(type(listaImutable))
