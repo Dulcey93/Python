7dcb1db56f61750ce94dd973440fa7273411bac7
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-02-15 18:23:09 -0500

    Ejercicio diez

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-# Construya un algoritmo en Python, que permita ingresar la
-# información de un empleado e imprima el nombre, los
-# apellidos y la antigüedad. Los datos que se deben solicitar
-# son los siguientes:
-# *Nombre * Teléfono *Año de ingreso a la empresa
-# *Apellidos *Edad.
-import datetime
+# 10. En su casa le solicitan que realice un algoritmo en Python,
+# que permita calcular el valor a pagar por concepto de
+# energía eléctrica. Los datos que se conocen son los
+# siguientes:
+# - Mes de consumo - Valor kw
+# -Total kw consumido en el mes - estrato
+
 option =0
-currentDateTime = datetime.datetime.now()
-date = currentDateTime.date()
-year = int(date.strftime("%Y"))
-class Person:
-    def __init__(self, nombre, apellido, edad, telefono, fecha_ingreso):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.telefono = telefono
-        self.fecha_ingreso = fecha_ingreso
+class Cuenta:
+    def __init__(self, mes, valorkw, consumokw, estrato):
+        self.mes = mes
+        self.valorkw = valorkw
+        self.consumokw =consumokw 
+        self.estrato = estrato
 
-    def imprimir(self):
-        antiguedad = year - int(self.fecha_ingreso)
-        return print(f"***===============> El empleado {self.nombre} {self.apellido} tiene {antiguedad} años en la empresa ///********** \t \n")
+    def calcularTotal(self):
+            if(self.estrato==1):
+                print(f"Valor total a pagar: {(self.consumokw * self.valorkw)*1.5}")
+            elif(self.estrato==2):
+                print(f"Valor total a pagar: {(self.consumokw * self.valorkw)*1.6}")
+            elif(self.estrato==3):
+                print(f"Valor total a pagar: {(self.consumokw * self.valorkw)*1.7}")
+            elif(self.estrato==4):
+                print(f"Valor total a pagar: {(self.consumokw * self.valorkw)*1.8}")
+            elif(self.estrato==5):
+                print(f"Valor total a pagar: {(self.consumokw * self.valorkw)*1.9}")
+            elif(self.estrato==6):
+                print(f"Valor total a pagar: {(self.consumokw * self.valorkw)*2.0}")
 
-while(option != 3):
-    option = int (input((f""" ====> MENÚ EMPLEADOS <====
-    \n 1) Ingresar empleado
-    \n 2) Imprimir empleado
-    \n 3) Salir
+while(option != 2):
+    option = int (input((f""" ====> BIENVENIDO AL MENÚ POR FAVOR ELIJA UNA OPCIÓN <====
+    \n 1) Calcular valor a pagar
+    \n 2) Salir
     \n""")))
 
     if(option==1):
-        print(f""" ====> INGRESAR EMPLEADO <====""")
-        nombre = input(f"Ingrese el nombre: ")
-        apellido = input(f"Ingrese el apellido: ")
-        edad = input(f"Ingrese la edad: ")
-        telefono = input(f"Ingrese el telefono: ")
-        fecha_ingreso = input(f"Ingrese la fecha de ingreso *EJEMPLO 2017*: ")
-        instancia = Person(nombre, apellido, edad, telefono, fecha_ingreso)
-    if(option==2):
-        instancia.imprimir()
+        print(f""" ====> CALCULE SU VALOR A PAGAR <====""")
+        mes = int (input(f"Ingresar mes de consumo *EJEMPLO: 1-2-3-4...*: "))
+        valorkw = int (input(f"Ingrese el valor por kw: "))
+        consumokw = int (input(f"Ingrese el número de kw consumidos en el mes: "))
+        estrato = int (input(f"Ingrese su estrato: "))
+        instancia = Cuenta(mes, valorkw, consumokw, estrato)
+        instancia.calcularTotal()
